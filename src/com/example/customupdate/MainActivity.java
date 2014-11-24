0b49d2264a5fbdb227c9126811c6f19bfe019d76
mashpy <mashpysays@gmail.com>
2014-11-24 15:59:21 +0600

    initial commit

---
+++ src/com/example/customupdate/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.customupdate;
+
+import com.winsontan520.wversionmanager.library.WVersionManager;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	    WVersionManager versionManager = new WVersionManager(this);
+	    versionManager.setVersionContentUrl("http://download.mashpy.me/version.txt"); // your update content url, see the response format below
+	    versionManager.checkVersion();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
